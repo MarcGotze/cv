e8af40a956b23eb694ececa9b14a5d96ac7b2480
Marc <marc.gotze@gmail.com>
2023-04-13 12:41:19 +0200

    Ajout des composants Header et Picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Picture from "./Picture";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <div className="title">
+        <h1>Développeur Junior d'application JS/React.js</h1>
+        <ul>
+          <li>Contrat recherché : CDD/CDI/FREELANCE</li>
+          <li>Organisme de formation : </li>
+          <li>Zone de recherche : Rhône ouest & Loire est</li>
+        </ul>
+      </div>
+      <Picture />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Picture = () => {
-  return <div></div>;
+  return (
+    <div className="picture-container">
+      <h1>Marc GÖTZE</h1>
+      <div className="portrait">
+        <img
+          className="photo"
+          src="assets/picture.png"
+          alt="Portrait de Marc Götze"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Picture;
