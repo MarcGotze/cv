e45e7e43100e39015adc3bde536465beabb2aa57
Marc <marc.gotze@gmail.com>
2023-04-25 17:45:18 +0200

    CV Complete design rework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,144 @@
 import React from "react";
-import Header from "../components/Header";
-import Experiences from "../components/Experiences";
-import Asides from "../components/Asides";
-import Footer from "../components/Footer";
-import Navigation from "../components/Navigation";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const Home = () => {
+  const titleRef = useRef();
+
+  const onLoad = () => {
+    gsap
+      .timeline()
+      .fromTo(
+        ".letter",
+        {
+          x: -100,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          stagger: 0.33,
+          delay: 0.7,
+        }
+      )
+      .to(".title", {
+        y: 45,
+        delay: 0.7,
+      });
+  };
+
+  const slideInTop = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        y: -200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: delay || 0.3,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        },
+      }
+    );
+  };
+
+  const slideInLeft = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        x: -200,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        delay: delay || 0.3,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  useEffect(() => {
+    slideInTop("#box1");
+  }, []);
+
+  useEffect(() => {
+    slideInTop("#box2");
+  }, []);
+
+  useEffect(() => {
+    slideInTop("#box3");
+  }, []);
+
+  useEffect(() => {
+    slideInLeft("#box4");
+  }, []);
+
+  useEffect(() => {
+    slideInLeft("#box5");
+  }, []);
+
   return (
-    <div className="wrapper">
-      <Header isHomePage={true} />
-      <Navigation />
-      <section className="intro">
-        <i className="animated-text">
-          "Après 12 années d’expérience professionnelle dans le milieu de la
-          planification, je souhaite aujourd’hui m’orienter vers le métier
-          passionnant de Développeur Web. Intègre, curieux et à l’écoute, j’aime
-          travailler en équipe, échanger les idées et contribuer à la résolution
-          de problématiques."
-        </i>
+    <div>
+      <h1 className="title" ref={titleRef}>
+        <span className="letter">M</span>
+        <span className="letter">A</span>
+        <span className="letter">R</span>
+        <span className="letter">C</span>
+        <span className="letter">/</span>
+        <span className="letter">G</span>
+        <span className="letter">Ö</span>
+        <span className="letter">T</span>
+        <span className="letter">Z</span>
+        <span className="letter">E</span>
+      </h1>
+      <section id="nextSection">
+        <div id="box1" className="box">
+          <h3>test 1</h3>
+        </div>
+        <div id="box2" className="box">
+          <h3>test 2</h3>
+        </div>
+        <div id="box3" className="box">
+          <h3>test 3</h3>
+        </div>
+        <div id="box4" className="box">
+          <h3>test 4</h3>
+        </div>
+        <div id="box5" className="box">
+          <h3>test 5</h3>
+        </div>
       </section>
-      <div className="rows-container">
-        <Experiences />
-        <Asides />
-      </div>
-      <Footer />
+
+      <section id="lastSection">
+        <div
+          id="box5"
+          className="box"
+          // onMouseEnter={onEnter}
+          // onMouseLeave={onLeave}
+        >
+          Last Section
+        </div>
+      </section>
     </div>
   );
 };
